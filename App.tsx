e1d2797f31d0bb59b84b9d8064129e2a6477ca50
Luiz Pires <lpiresjunior@gmail.com>
2023-07-05 02:43:51 -0300

    chore: install and config realmdb

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
 import { StatusBar } from 'react-native'
 import { ThemeProvider } from 'styled-components/native'
 
+import { REALM_APP_ID } from '@env'
 import {
   Roboto_400Regular,
   Roboto_700Bold,
   useFonts,
 } from '@expo-google-fonts/roboto'
+import { AppProvider, UserProvider } from '@realm/react'
 
 import { Loading } from './src/ components/Loading'
+import { Home } from './src/screens/Home'
 import { SignIn } from './src/screens/SignIn'
 import theme from './src/theme'
 
@@ -19,13 +22,17 @@ export default function App() {
   }
 
   return (
-    <ThemeProvider theme={theme}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      <SignIn />
-    </ThemeProvider>
+    <AppProvider id={REALM_APP_ID}>
+      <ThemeProvider theme={theme}>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        <UserProvider fallback={SignIn}>
+          <Home />
+        </UserProvider>
+      </ThemeProvider>
+    </AppProvider>
   )
 }
