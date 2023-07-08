82156aa433d026eb984bdc9f0ada40c2bc4b22ca
Luiz Pires <lpiresjunior@gmail.com>
2023-07-08 04:29:02 -0300

    feat: validate license plate

--- src/ components/LicensePlateInput/index.tsx
+++ src/ components/LicensePlateInput/index.tsx
@@ -1,23 +1,31 @@
-import { TextInputProps } from 'react-native'
+import React, { forwardRef } from 'react'
+import { TextInput, TextInputProps } from 'react-native'
 
 import theme from '../../theme'
 import { Container, Input, Label } from './styles'
 
 type Props = TextInputProps & {
   label: string
+  ref?: React.Ref<TextInput>
 }
 
-export function LicensePlateInput({ label, ...rest }: Props) {
-  return (
-    <Container>
-      <Label>{label}</Label>
+const LicensePlateInput = forwardRef<TextInput, Props>(
+  ({ label, ...rest }, ref) => {
+    return (
+      <Container>
+        <Label>{label}</Label>
 
-      <Input
-        maxLength={7}
-        autoCapitalize="characters"
-        placeholderTextColor={theme.colors.gray_400}
-        {...rest}
-      />
-    </Container>
-  )
-}
+        <Input
+          ref={ref}
+          maxLength={7}
+          autoCapitalize="characters"
+          placeholderTextColor={theme.colors.gray_400}
+          {...rest}
+        />
+      </Container>
+    )
+  }
+)
+
+LicensePlateInput.displayName = 'LicensePlateInput'
+export { LicensePlateInput }
