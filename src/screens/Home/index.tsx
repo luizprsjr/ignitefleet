2e85bdb94e636b9fbe7c5c9cb38494b153b0cac9
Luiz Pires <lpiresjunior@gmail.com>
2023-07-09 13:01:53 -0300

    feat: fetch vehicle

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,29 @@
+import { useEffect } from 'react'
+
 import { useNavigation } from '@react-navigation/native'
 
 import { CarStatus } from '../../ components/CarStatus'
 import { HomeHeader } from '../../ components/HomeHeader'
+import { useQuery } from '../../libs/realm'
+import { Historic } from '../../libs/realm/schemas/historic'
 import { Container, Content } from './styles'
 
 export function Home() {
   const { navigate } = useNavigation()
 
+  const historic = useQuery(Historic)
+
   function handleRegisterMovement() {
     navigate('departure')
   }
+
+  function fetchVehicle() {
+    console.log(historic)
+  }
+
+  useEffect(() => {
+    fetchVehicle()
+  }, [])
 
   return (
     <Container>
