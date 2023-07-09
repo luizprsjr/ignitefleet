58f48991c4912123e12cffbefc420f5f42f29c12
Luiz Pires <lpiresjunior@gmail.com>
2023-07-09 12:56:03 -0300

    feat: register the departure of the vehicle

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -7,10 +7,15 @@ import {
   TextInput,
 } from 'react-native'
 
+import { useNavigation } from '@react-navigation/native'
+import { useUser } from '@realm/react'
+
 import { Button } from '../../ components/Button'
 import { Header } from '../../ components/Header'
 import { LicensePlateInput } from '../../ components/LicensePlateInput'
 import { TextAreaInput } from '../../ components/TextAreaInput'
+import { useRealm } from '../../libs/realm'
+import { Historic } from '../../libs/realm/schemas/historic'
 import { licensePlateValidate } from '../../utils/licensePlateValidate'
 import { Container, Content } from './styles'
 
@@ -20,25 +25,52 @@ const keyBoardAvoidingViewBehavior =
 export function Departure() {
   const [description, setDescription] = useState('')
   const [licensePlate, setLicensePlate] = useState('')
+  const [isRegistering, setIsRegistering] = useState(false)
+
+  const { goBack } = useNavigation()
+  const realm = useRealm()
+  const user = useUser()
 
   const descriptionRef = useRef<TextInput>(null)
   const licensePlateRef = useRef<TextInput>(null)
 
   function handleDepartureRegister() {
-    if (!licensePlateValidate(licensePlate)) {
-      licensePlateRef.current?.focus()
-      return Alert.alert(
-        'Placa inválida',
-        'A placa é inválida. Por favor informe a placa correta do veículo'
-      )
-    }
+    try {
+      if (!licensePlateValidate(licensePlate)) {
+        licensePlateRef.current?.focus()
+        return Alert.alert(
+          'Placa inválida',
+          'A placa é inválida. Por favor informe a placa correta do veículo'
+        )
+      }
 
-    if (description.trim().length === 0) {
-      descriptionRef.current?.focus()
-      return Alert.alert(
-        'Finalidade',
-        'Por favor, informe a finalidade da utilização do veículo'
-      )
+      if (description.trim().length === 0) {
+        descriptionRef.current?.focus()
+        return Alert.alert(
+          'Finalidade',
+          'Por favor, informe a finalidade da utilização do veículo'
+        )
+      }
+
+      setIsRegistering(true)
+
+      realm.write(() => {
+        realm.create(
+          'Historic',
+          Historic.generate({
+            user_id: user?.id,
+            license_plate: licensePlate.toUpperCase(),
+            description,
+          })
+        )
+      })
+
+      Alert.alert('Saída', 'Saída do veículo registrada com sucesso.')
+      goBack()
+    } catch (error) {
+      console.log(error)
+      Alert.alert('Erro', 'Não foi possível registrar a saída do veículo.')
+      setIsRegistering(false)
     }
   }
 
@@ -71,7 +103,11 @@ export function Departure() {
               onChangeText={setDescription}
             />
 
-            <Button title="Registrar Saída" onPress={handleDepartureRegister} />
+            <Button
+              title="Registrar Saída"
+              onPress={handleDepartureRegister}
+              isLoading={isRegistering}
+            />
           </Content>
         </ScrollView>
       </KeyboardAvoidingView>
