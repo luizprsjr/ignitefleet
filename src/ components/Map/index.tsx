043af148b24bfdb7da474246e05ce88385cae7aa
Luiz Pires <lpiresjunior@gmail.com>
2023-07-12 18:55:30 -0300

    adjust the map to fit the two pins

--- src/ components/Map/index.tsx
+++ src/ components/Map/index.tsx
@@ -1,4 +1,5 @@
-import { Car } from 'phosphor-react-native'
+import { Car, FlagCheckered } from 'phosphor-react-native'
+import { useRef } from 'react'
 import MapView, {
   LatLng,
   MapViewProps,
@@ -13,10 +14,20 @@ type Props = MapViewProps & {
 }
 
 export function Map({ coordinates, ...rest }: Props) {
+  const mapRef = useRef<MapView>(null)
   const lastCoordinate = coordinates[coordinates.length - 1]
+
+  async function onMapLoaded() {
+    if (coordinates.length > 1) {
+      mapRef.current?.fitToSuppliedMarkers(['departure', 'arrival'], {
+        edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
+      })
+    }
+  }
 
   return (
     <MapView
+      ref={mapRef}
       provider={PROVIDER_GOOGLE}
       style={{ width: '100%', height: 200 }}
       region={{
@@ -25,11 +36,18 @@ export function Map({ coordinates, ...rest }: Props) {
         latitudeDelta: 0.005,
         longitudeDelta: 0.005,
       }}
+      onMapLoaded={onMapLoaded}
       {...rest}
     >
-      <Marker coordinate={coordinates[0]}>
+      <Marker identifier="departure" coordinate={coordinates[0]}>
         <IconBox icon={Car} size="SMALL" />
       </Marker>
+
+      {coordinates.length > 1 && (
+        <Marker identifier="arrival" coordinate={lastCoordinate}>
+          <IconBox icon={FlagCheckered} size="SMALL" />
+        </Marker>
+      )}
     </MapView>
   )
 }
--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -146,7 +146,14 @@ export function Departure() {
 
       <KeyboardAwareScrollView>
         <ScrollView>
-          {currentCoords && <Map coordinates={[currentCoords]} />}
+          {currentCoords && (
+            <Map
+              coordinates={[
+                { latitude: -23.5657, longitude: -46.6515 },
+                { latitude: -23.5694, longitude: -46.6467 },
+              ]}
+            />
+          )}
           <Content>
             {currentAddress && (
               <LocationInfo
